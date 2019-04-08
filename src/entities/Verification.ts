b48ae3a91cd7e48237967175a07d21036ae26bac
KIMINJOONG <ijboym216@gmail.com>
2019-04-08 21:29:00 +0900

    Creating the Verification Key

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -1,31 +1,47 @@
 import {
-    BaseEntity,
-    Column, 
-    CreateDateColumn, 
-    Entity, 
-    PrimaryColumn, 
-    UpdateDateColumn, 
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+  BeforeInsert
 } from "typeorm";
 import { verificationTarget } from "src/types/Types";
 
+const PHONE = "PHONE";
+const EMAIL = "EMAIL";
+
 @Entity()
 class Verification extends BaseEntity {
-    @PrimaryColumn() id: number
-    
-    @Column({type: "text", enum: ["PHONE", "EMAIL"]})
-    target: verificationTarget;
+  @PrimaryColumn() id: number;
 
-    @Column({type: "text"})
-    payload: string;
+  @Column({ type: "text", enum: [PHONE, EMAIL] })
+  target: verificationTarget;
 
-    @Column({type: "text"})
-    key: string;
+  @Column({ type: "text" })
+  payload: string;
 
-    @Column({type: "boolean", default: false})
-    used: boolean;
+  @Column({ type: "text" })
+  key: string;
 
-    @CreateDateColumn() createdAt: string;
-    @UpdateDateColumn() updatedAt: string;
+  @Column({ type: "boolean", default: false })
+  used: boolean;
+
+  @CreateDateColumn() createdAt: string;
+  @UpdateDateColumn() updatedAt: string;
+
+  // Insert를 하기전에 target을 구분한후 랜덤으로 key를 만들어주는 함수
+  @BeforeInsert()
+  createKey(): void {
+    if (this.target === PHONE) {
+      this.key = Math.floor(Math.random() * 100000).toString();
+    } else if (this.target === EMAIL) {
+      this.key = Math.random()
+        .toString(36)
+        .substr(2);
+    }
+  }
 }
 
 export default Verification;
