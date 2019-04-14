e7952e84420ca817876e5a0191c103c9e11d40f5
KIMINJOONG <ijboym216@gmail.com>
2019-04-14 12:28:48 +0900

    Custom Auth Middleware on Express part ONe

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,30 @@
 import cors from "cors";
-import {GraphQLServer} from "graphql-yoga";
+import { GraphQLServer } from "graphql-yoga";
 import helmet from "helmet";
 import logger from "morgan";
 import schema from "./schema";
 
 class App {
-    public app: GraphQLServer;
-    constructor() {
-        this.app = new GraphQLServer({
-            schema
-        });
-        this.middlewares();
+  public app: GraphQLServer;
+  constructor() {
+    this.app = new GraphQLServer({
+      schema
+    });
+    this.middlewares();
+  }
+  private middlewares = (): void => {
+    this.app.express.use(cors());
+    this.app.express.use(logger("dev"));
+    this.app.express.use(helmet());
+  };
+
+  // token을 여는 함수
+  private jwt = async (req, res, next): Promise<void> => {
+    const token = req.get("X-JWT");
+    if(token){
+        
     }
-    private middlewares = () : void => {
-        this.app.express.use(cors());
-        this.app.express.use(logger("dev"));
-        this.app.express.use(helmet());
-    };
+  };
 }
 
 export default new App().app;
