cd426dda2fce6301135ce007030107a3269ae3e7
KIMINJOONG <ijboym216@gmail.com>
2019-04-15 06:18:21 +0900

    Sending Confirmation Email part Three

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -4,3 +4,19 @@ const mailGunClient = new Mailgun({
   apiKey: process.env.MAILGUN_API_KEY || "",
   domain: "sandbox9a5a9f0a85f0431d88c12643de014447.mailgun.org"
 });
+
+const sendEmail = (subject: string, html: string) => {
+  const emailData: Mailgun.messages.SendData = {
+    from: "ijboym216@gmail.com",
+    to: "ijboym216@gamil.com",
+    subject,
+    html
+  };
+  return mailGunClient.messages().send(emailData);
+};
+
+export const sendVerificationEmail = (fullName: string, key: string) => {
+  const emailSubject = `Hello! ${fullName}, please verify your email`;
+  const emailBody = `Verify your email by clicking <a href="http://nuber.com/verification/${key}/">here</a>`;
+  return sendEmail(emailSubject, emailBody);
+};
