ef4a9729973d9b935f9400362b13693f8922d272
KIMINJOONG <ijboym216@gmail.com>
2019-04-16 21:38:39 +0900

    RequestEmailVerification Resolver

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -2,13 +2,14 @@ import Mailgun from "mailgun-js";
 
 const mailGunClient = new Mailgun({
   apiKey: process.env.MAILGUN_API_KEY || "",
-  domain: "sandbox9a5a9f0a85f0431d88c12643de014447.mailgun.org"
+  domain:
+    "https://api.mailgun.net/v3/sandbox9a5a9f0a85f0431d88c12643de014447.mailgun.org"
 });
 
 const sendEmail = (subject: string, html: string) => {
   const emailData: Mailgun.messages.SendData = {
     from: "ijboym216@gmail.com",
-    to: "ijboym216@gamil.com",
+    to: "dlswnd4452@naver.com",
     subject,
     html
   };
