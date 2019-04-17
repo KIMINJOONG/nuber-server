bf5688a95475088085d0833bd52a6109fb443497
KIMINJOONG <ijboym216@gmail.com>
2019-04-17 21:32:19 +0900

    UpdateMyProfile Resolver part Two

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -2,14 +2,13 @@ import Mailgun from "mailgun-js";
 
 const mailGunClient = new Mailgun({
   apiKey: process.env.MAILGUN_API_KEY || "",
-  domain:
-    "https://api.mailgun.net/v3/sandbox9a5a9f0a85f0431d88c12643de014447.mailgun.org"
+  domain: "sandbox9a5a9f0a85f0431d88c12643de014447.mailgun.org"
 });
 
 const sendEmail = (subject: string, html: string) => {
   const emailData: Mailgun.messages.SendData = {
     from: "ijboym216@gmail.com",
-    to: "dlswnd4452@naver.com",
+    to: "ijboym216@gmail.com",
     subject,
     html
   };
