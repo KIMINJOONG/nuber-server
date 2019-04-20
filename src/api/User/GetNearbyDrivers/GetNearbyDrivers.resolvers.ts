886f874d4167893cc27e6f43eec117a2c632d0c4
KIMINJOONG <ijboym216@gmail.com>
2019-04-20 10:17:50 +0900

    GetNearbyDrivers Resolver part Two

--- src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
+++ src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
@@ -1,7 +1,7 @@
 import { Resolvers } from "../../../types/resolvers";
 import privateResolver from "../../../utils/resolverMiddleware";
 import User from "../../../entities/User";
-import { Between } from "typeorm";
+import { Between, getRepository } from "typeorm";
 import { GetNearbyDriversResponse } from "src/types/graph";
 
 const resolvers: Resolvers = {
@@ -10,11 +10,24 @@ const resolvers: Resolvers = {
       async (_, __, { req }): Promise<GetNearbyDriversResponse> => {
         const user: User = req.user;
         const { lastLat, lastLng } = user;
-        const drivers = await User.find({
-          isDriving: true,
-          lastLat: Between(lastLat - 0.05, lastLat + 0.05),
-          lastLng: Between(lastLng - 0.05, lastLng + 0.05)
-        });
+        try {
+          const drivers = await getRepository(User).find({
+            isDriving: true,
+            lastLat: Between(lastLat - 0.05, lastLat + 0.05),
+            lastLng: Between(lastLng - 0.05, lastLng + 0.05)
+          });
+          return {
+            ok: true,
+            error: null,
+            drivers
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            drivers: null
+          };
+        }
       }
     )
   }
