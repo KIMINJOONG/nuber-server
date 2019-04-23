9a7047e36eb7ac3933cb61360cf645918c69fb9a
KIMINJOONG <ijboym216@gmail.com>
2019-04-23 20:38:41 +0900

    NearbyRideSubscription

---
+++ src/api/Ride/NearbyRideSubscription/NearbyRideSubscription.resolvers.ts
@@ -0,0 +1,27 @@
+import { withFilter } from "graphql-yoga";
+import User from "../../../entities/User";
+
+const resolvers = {
+  Subscription: {
+    NearbyRideSubscription: {
+      subscribe: withFilter(
+        (_, __, { pubSub }) => pubSub.asyncIterator("driverUpdate"), // driverUpdate는 내가 임의로 이름을 정한것
+        (payload, _, { context }) => {
+          const user: User = context.currentUser;
+          const {
+            NearbyRideSubscription: { pickUpLat, pickUpLng }
+          } = payload;
+          const { lastLat: userLastLat, lastLng: userLastLng } = user;
+          return (
+            pickUpLat >= userLastLat - 0.05 &&
+            pickUpLat <= userLastLat + 0.05 &&
+            pickUpLng >= userLastLng - 0.05 &&
+            pickUpLng <= userLastLng + 0.05
+          );
+        }
+      )
+    }
+  }
+};
+
+export default resolvers;
